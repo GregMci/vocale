0e6e21fe6839837377009eab2fbf4c315904a900
Greg McInerny <g.mcinerny@warwick.ac.uk>
2023-09-08 10:55:22 +0100

    removing code for local tests

--- R/open_pdf_from_R_code.R
+++ R/open_pdf_from_R_code.R
@@ -1,14 +1,4 @@
 
-#pathToR <- .libPaths()[1]
-
-#pathToR <- "C:/Users/u1473392/Documents/Warwick - CIM/Anti-Tufte/vocale_R_package/"
-
-# target directory/file...
-# 'open "C:/Users/u1473392/Documents/Warwick - CIM/Anti-Tufte/vocale_cheat_sheet_test_axis.pdf"'
-#pathToFile <- paste( "\'open \"", pathToR, "vocale_cheat_sheet_test_axis.pdf\"\' ", sep="" )
-
-# system( eval( parse( text=pathToFile ) ) )
-
 cheat_sheet_axis <- function(){
   pathToR <- paste( find.package("vocale") , "/data/", sep="" )
   pathToFile <- paste( "\'open \"", pathToR, "vocale_cheat_sheet_test_axis.pdf\"\' ", sep="" )
